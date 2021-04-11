f31c94e1b272953a2adf2c7b845f146e0720651f
Tralcan <tralcanx@protonmail.com>
2021-04-11 01:51:50 -0300

    gh-pages

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,7 @@ import React from "react";
 function Footer() {
     return(
         <footer id="footer">
-            <p class="copyright">&copy; <a href="https://github.com/pungtas">Tyron Pungtas</a></p>
-            <p class="copyright">Dapp hosted on the <a href="https://www.arweave.org/">permaweb</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+            <p class="copyright">&copy; <a href="https://github.com/pungtas">Tyron Pungtas</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
 		</footer>
     );
 }
