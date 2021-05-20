a7bd090f94898a73d65253309429a1884f49da09
Dorian30 <alvaradogdorian@gmail.com>
2021-05-20 18:27:23 -0300

    Implemented providers in the root of the project

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useReducer } from "react";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+import {
+  stateContext as StateContext,
+  dispatchContext as DispatchContext,
+  globalReducer,
+  globalState,
+} from "./context/index";
+import Routes from "./constants/routes";
+import { Header, Footer, Home, Domains, Contact } from "./components";
+import "./styles/css/main.css";
+
+function App() {
+  const [state, dispatch] = useReducer(globalReducer, globalState);
+  return (
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <div id="wrapper">
+          <Router>
+            <Header />
+            <Switch>
+              <Route exact path={Routes.Home} component={Home} />
+              <Route exact path={Routes.Domains} component={Domains} />
+              <Route exact path={Routes.Contact} component={Contact} />
+              <Redirect to={Routes.Home} />
+            </Switch>
+            <Footer />
+          </Router>
+        </div>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
+  );
+}
+
+export default App;
