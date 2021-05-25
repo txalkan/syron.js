261617d001b819d70438301ca9b9a3ed4e88cbf0
Dorian30 <alvaradogdorian@gmail.com>
2021-05-25 20:27:26 -0300

    Added smart contracts urls and supported domains

---
+++ src/constants/tyron.ts
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://github.com/pungtas/smart-contracts.tyron/blob/main';
+
+// @TODO: Add missing urls from this list.
+export const SMART_CONTRACTS_URLS = {
+  init: `${BASE_URL}/INIT/init.tyron.scilla`,
+  did: `${BASE_URL}/DID/did.tyron.scilla`,
+  ssi: `${BASE_URL}/SSI/ssi.tyron.scilla`,
+  pst: `${BASE_URL}/PST/pst.tyron.scilla`,
+  pwallet: `${BASE_URL}/PERMAWALLET/pWallet.tyron.js`
+};
