b9dcf7dd147c981e0c6902e65706e3d8dc21a549
Dorian30 <alvaradogdorian@gmail.com>
2021-05-26 13:27:17 -0300

    Modified search bar behaviour

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,17 +1,38 @@
 import React, { useState } from "react";
 
-import { SMART_CONTRACTS_URLS, VALID_SMART_CONTRACTS } from "../../constants/tyron";
+import {
+  SMART_CONTRACTS_URLS,
+  VALID_SMART_CONTRACTS,
+} from "../../constants/tyron";
 import { DOMAINS } from "../../constants/domains";
 
 import styles from "./styles.module.scss";
 
 function SearchBar() {
   const [value, setValue] = useState("");
+  const [name, setName] = useState("");
+  const [domain, setDomain] = useState("");
   const [error, setError] = useState("");
 
   const handleOnKeyPress = ({ key }: React.KeyboardEvent<HTMLInputElement>) => {
     if (key === "Enter") {
-      console.log("Do magic stuff here");
+      // @TODO: Handle other domains
+      switch (domain) {
+        case DOMAINS.TYRON:
+          if (VALID_SMART_CONTRACTS.includes(name))
+            window.open(
+              SMART_CONTRACTS_URLS[
+                name as unknown as keyof typeof SMART_CONTRACTS_URLS
+              ]
+            );
+          else setError("Invalid smart contract");
+          break;
+        case DOMAINS.SSI:
+          console.log("");
+          break;
+        default:
+          setError("Domain not valid");
+      }
     }
   };
 
@@ -19,29 +40,17 @@ function SearchBar() {
     currentTarget: { value },
   }: React.ChangeEvent<HTMLInputElement>) => {
     setValue(value);
-    const [name, domain] = value.split(".");
-
-    // @TODO: Handle other domains
-    switch (domain) {
-      case DOMAINS.TYRON:
-        if (VALID_SMART_CONTRACTS.includes(name))
-          window.open(
-            SMART_CONTRACTS_URLS[
-              name as unknown as keyof typeof SMART_CONTRACTS_URLS
-            ]);
-        else 
-          setError('Invalid smart contract');
-        break;
-      case DOMAINS.SSI:
-        console.log('');
-        break;
-      default:
-        setError('Domain not valid');
+    if (value) {
+      const [name = "", domain = ""] = value.split(".");
+      setName(name);
+      setDomain(domain);
+    } else {
+      setError("");
     }
   };
 
   return (
-    <>
+    <div className={styles.container}>
       <input
         type="text"
         className={styles.searchBar}
@@ -50,7 +59,7 @@ function SearchBar() {
         value={value}
       />
       <p className={styles.errorMsg}>{error}</p>
-    </>
+    </div>
   );
 }
 
