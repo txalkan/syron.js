f19646456c9a116c4fe6c9e4d4b213f8e497c904
Dorian30 <alvaradogdorian@gmail.com>
2021-06-01 10:21:30 -0300

    Added some config to the project:
    - ESLint source file for code quality.
    - Prettier source file for code style.
    - Husky for git hooks. Set pre-commit script for running typescript and linters before even being able to commit.
    - Added VSCode workspace settings.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+};
