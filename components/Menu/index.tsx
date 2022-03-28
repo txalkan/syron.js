e525a0cb4e03a1643afceb2aa780839a2da35996
Ilham Bagas <ilhambagas@Ilhams-MacBook-Pro.local>
2022-03-28 10:18:30 +0700

    task/close menu when clicking outside of the menu#2

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -57,7 +57,7 @@ function Component(props: Props) {
   }
 
   return (
-    <div>
+    <>
       <ConnectModal />
       <NewSSIModal />
       <TransactionStatus />
@@ -66,41 +66,44 @@ function Component(props: Props) {
           <Image alt="menu-ico" width={25} height={25} src={menu} />
         </div>
       ) : (
-        <div className={styles.menu}>
-          <div onClick={() => { updateMenuOn(false); setActiveMenu("") }} className={styles.back}>
-            <Image alt="back-ico" width={25} height={25} src={back} />
+        <>
+          <div className={styles.outerWrapper} onClick={() => { updateMenuOn(false); setActiveMenu("") }} />
+          <div className={styles.menu}>
+            <div onClick={() => { updateMenuOn(false); setActiveMenu("") }} className={styles.back}>
+              <Image alt="back-ico" width={25} height={25} src={back} />
+            </div>
+            <div className={styles.menuItemWrapper}>
+              <h3 onClick={showConnectModal} className={styles.menuItemText}>CONNECT</h3>
+              <h3 onClick={() => { dispatchShowSSIModal(); updateMenuOn(false) }} className={styles.menuItemText}>NEW SSI</h3>
+              {
+                activeMenu !== "ssiprotocol" ? (
+                  <h3 onClick={() => setActiveMenu("ssiprotocol")} className={styles.menuItemText}>SSI PROTOCOl</h3>
+                ) : activeMenu === "ssiprotocol" && (
+                  <>
+                    <h3 onClick={() => setActiveMenu("")} className={styles.menuItemTextActive}>SSI PROTOCOl</h3>
+                    <div className={styles.subMenuItemWrapper}>
+                      <div onClick={() => window.open("https://www.ssiprotocol.com/#/about")} className={styles.subMenuItemListWrapper}>
+                        <text className={styles.subMenuItemListText}>About</text>
+                      </div>
+                      <div onClick={() => window.open("https://www.ssiprotocol.com/#/contact")} className={styles.subMenuItemListWrapper}>
+                        <text className={styles.subMenuItemListText}>Contact</text>
+                      </div>
+                      <div onClick={() => window.open("https://www.ssiprotocol.com/#/wallets")} className={styles.subMenuItemListWrapper}>
+                        <text className={styles.subMenuItemListText}>DIDxWallet</text>
+                      </div>
+                      <div onClick={() => window.open("https://ssiprotocol.notion.site/TYRON-Whitepaper-5ca16fc254b343fb90cfeb725cbfa2c3")} className={styles.subMenuItemListWrapper}>
+                        <text className={styles.subMenuItemListText}>Whitepaper</text>
+                      </div>
+                    </div>
+                  </>
+                )
+              }
+              <h3 onClick={() => window.open("https://ssiprotocol.notion.site/Frequently-Asked-Questions-6163a4186d874e90b2316d4cd827710c")} className={styles.menuItemText}>FAQ</h3>
+            </div>
           </div>
-          <div className={styles.menuItemWrapper}>
-            <h3 onClick={showConnectModal} className={styles.menuItemText}>CONNECT</h3>
-            <h3 onClick={() => { dispatchShowSSIModal(); updateMenuOn(false) }} className={styles.menuItemText}>NEW SSI</h3>
-            {
-              activeMenu !== "ssiprotocol" ? (
-                <h3 onClick={() => setActiveMenu("ssiprotocol")} className={styles.menuItemText}>SSI PROTOCOl</h3>
-              ) : activeMenu === "ssiprotocol" && (
-                <>
-                  <h3 onClick={() => setActiveMenu("")} className={styles.menuItemTextActive}>SSI PROTOCOl</h3>
-                  <div className={styles.subMenuItemWrapper}>
-                    <div onClick={() => window.open("https://www.ssiprotocol.com/#/about")} className={styles.subMenuItemListWrapper}>
-                      <text className={styles.subMenuItemListText}>About</text>
-                    </div>
-                    <div onClick={() => window.open("https://www.ssiprotocol.com/#/contact")} className={styles.subMenuItemListWrapper}>
-                      <text className={styles.subMenuItemListText}>Contact</text>
-                    </div>
-                    <div onClick={() => window.open("https://www.ssiprotocol.com/#/wallets")} className={styles.subMenuItemListWrapper}>
-                      <text className={styles.subMenuItemListText}>DIDxWallet</text>
-                    </div>
-                    <div onClick={() => window.open("https://ssiprotocol.notion.site/TYRON-Whitepaper-5ca16fc254b343fb90cfeb725cbfa2c3")} className={styles.subMenuItemListWrapper}>
-                      <text className={styles.subMenuItemListText}>Whitepaper</text>
-                    </div>
-                  </div>
-                </>
-              )
-            }
-            <h3 onClick={() => window.open("https://ssiprotocol.notion.site/Frequently-Asked-Questions-6163a4186d874e90b2316d4cd827710c")} className={styles.menuItemText}>FAQ</h3>
-          </div>
-        </div>
+        </>
       )}
-    </div>
+    </>
   );
 }
 
