fc327e9f6c97c5310c5fa0f0c29911737c944eed
DEV PH <dev.ph@wistkey.com>
2022-04-08 17:02:38 +0700

    task/add tx status for New SSI modal

--- components/DeployDid/index.tsx
+++ components/DeployDid/index.tsx
@@ -2,12 +2,22 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useStore } from "effector-react";
 import { toast } from "react-toastify";
+import { HTTPProvider } from "@zilliqa-js/core";
+import { Transaction } from "@zilliqa-js/account";
+import { BN, Long } from "@zilliqa-js/util";
+import { randomBytes, toChecksumAddress } from "@zilliqa-js/crypto";
 import { $zil_address } from "../../src/store/zil_address";
 import { ZilPayBase } from "../ZilPay/zilpay-base";
 import * as zcrypto from "@zilliqa-js/crypto";
 import { $new_ssi, updateNewSSI } from "../../src/store/new-ssi";
 import { $net } from "../../src/store/wallet-network";
-import { hideNewSSIModal } from "../../src/app/actions";
+import {
+  setTxStatusLoading,
+  showTxStatusModal,
+  setTxId,
+  hideTxStatusModal,
+  hideNewSSIModal,
+} from "../../src/app/actions";
 
 function Component() {
   const dispatch = useDispatch();
@@ -20,36 +30,81 @@ function Component() {
     if (zil_address !== null && net !== null) {
       setLoading(true);
       const zilpay = new ZilPayBase();
+
+      const generateChecksumAddress = () => toChecksumAddress(randomBytes(20));
+      let endpoint = "https://api.zilliqa.com/";
+      if (net === "testnet") {
+        endpoint = "https://dev-api.zilliqa.com/";
+      }
+      let tx = new Transaction(
+        {
+          version: 0,
+          toAddr: generateChecksumAddress(),
+          amount: new BN(0),
+          gasPrice: new BN(1000),
+          gasLimit: Long.fromNumber(1000),
+        },
+        new HTTPProvider(endpoint)
+      );
+      dispatch(hideNewSSIModal());
+      dispatch(setTxStatusLoading("true"));
+      dispatch(showTxStatusModal());
+
       await zilpay
         .deployDid(net, zil_address.base16)
-        .then((deploy: any) => {
-          let new_ssi = deploy[1].address;
-          new_ssi = zcrypto.toChecksumAddress(new_ssi);
-          updateNewSSI(new_ssi);
-          /** @todo-checked
-           * wait until contract deployment gets confirmed
-           * add spinner
-           * */
-          setLoading(false);
-          /**
-           * @todo-checked close New SSI modal so the user can see the search bar and the following message.
-           */
-          dispatch(hideNewSSIModal());
-          toast.info(
-            "Success! Search for the NFT Username you would like to buy for your SSI in the browser.",
-            {
-              position: "top-center",
-              autoClose: 6000,
+        .then(async (deploy: any) => {
+          dispatch(setTxId(deploy[0].ID));
+          dispatch(setTxStatusLoading("submitted"));
+
+          tx = await tx.confirm(deploy[0].ID);
+          if (tx.isConfirmed()) {
+            dispatch(setTxStatusLoading("confirmed"));
+            setTimeout(() => {
+              window.open(
+                `https://viewblock.io/zilliqa/tx/${deploy[0].ID}?network=${net}`
+              );
+            }, 1000);
+            let new_ssi = deploy[1].address;
+            new_ssi = zcrypto.toChecksumAddress(new_ssi);
+            updateNewSSI(new_ssi);
+            /** @todo-checked
+             * wait until contract deployment gets confirmed
+             * add spinner
+             * */
+            setLoading(false);
+            /**
+             * @todo-checked close New SSI modal so the user can see the search bar and the following message.
+             */
+            toast.info(
+              "Success! Search for the NFT Username you would like to buy for your SSI in the browser.",
+              {
+                position: "top-center",
+                autoClose: 6000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+              }
+            );
+          } else if (tx.isRejected()) {
+            dispatch(hideTxStatusModal());
+            dispatch(setTxStatusLoading("idle"));
+            toast.error("Transaction failed.", {
+              position: "top-right",
+              autoClose: 3000,
               hideProgressBar: false,
               closeOnClick: true,
               pauseOnHover: true,
               draggable: true,
               progress: undefined,
               theme: "dark",
-            }
-          );
+            });
+          }
         })
         .catch((error) => {
+          dispatch(setTxStatusLoading("idle"));
           setLoading(false);
           toast.error(String(error), {
             position: "top-right",
