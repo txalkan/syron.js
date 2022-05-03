68b3b829afed25c5ad8eb85570e94b9a31a3f4cc
tralkan <tralkan@ssiprotocol.com>
2022-05-02 23:34:49 -0300

    update get started

--- components/Modals/GetStartedModal/index.tsx
+++ components/Modals/GetStartedModal/index.tsx
@@ -78,7 +78,7 @@ function Component() {
                       <Image alt="point-1" src={c1} width={25} height={25} />
                     )}
                   </div>
-                  <p className={styles.rowHeaderTitle}>Create ZilPay Wallet</p>
+                  <p className={styles.rowHeaderTitle}>Zilliqa</p>
                 </div>
                 <div>
                   {active === 1 ? (
@@ -102,8 +102,7 @@ function Component() {
                 {active === 1 ? (
                   <>
                     <p className={styles.rowContentTxt}>
-                      To create your ZilPay wallet, you must first download the
-                      extension to your browser.
+                      Connect your Externally Owned Account for Zilliqa
                     </p>
                     <p className={styles.rowContentTxt}>
                       <br />
@@ -117,15 +116,18 @@ function Component() {
                             target="_blank"
                             rel="noreferrer"
                           >
-                            https://zilpay.io/
+                            zilpay.io
                           </a>{" "}
-                          and click on **GET CHROME EXTENSION**.
+                          and click on <strong>GET CHROME EXTENSION</strong>
                         </li>
                       </ul>
-                      Once you have downloaded and installed the extension, you
-                      will find an icon in the top right corner where Chrome
-                      extensions are listed. The ZilPay icon and name should be
-                      displayed in the list.
+                      Once you have installed the extension, get into it
+                      and click <strong>Create</strong> to generate a new account.
+                      First, you will see a list of words that make up
+                      your secret phrase. You have to write down these
+                      words in a safe place. Remember that these words
+                      must be in order and spelt correctly. You
+                      can choose 12 or 24 words.
                       <br />
                       <br />
                       <ul className={styles.ul}>
@@ -141,14 +143,9 @@ function Component() {
                             className={styles.outerWrapper}
                           />
                           <div className={styles.modalInfo}>
-                            <h5 className={styles.modalInfoTitle}>WARNING</h5>
+                            <h5 className={styles.modalInfoTitle}>INFO</h5>
                             <p>
-                              Although the words shown at the beginning could be
-                              8, your secret phrase contains 12 or 24 words. To
-                              see the complete list, click between the words in
-                              the list and press the down arrow button
-                              repeatedly on your keyboard until you see the
-                              total number of words.
+                              Although the words shown at the beginning are 8, your secret phrase is made up of 12 or 24 words. To see the complete list, click between the words in the list and press the down-arrow button repeatedly on your keyboard until you see the total number of words.
                             </p>
                           </div>
                         </>
@@ -159,7 +156,7 @@ function Component() {
                           phrase. You must write these words down in a safe
                           place. Remember that the words must be ordered and
                           spelt correctly. You can choose between 12 and 24
-                          words.
+                          words
                           <Image
                             onClick={() => setModalInfo(!modalInfo)}
                             alt="warning-ico"
@@ -171,126 +168,117 @@ function Component() {
                       </ul>
                       <ul className={styles.ul}>
                         <li className={styles.li}>
-                          Once you have entered all the words in your secret
-                          phrase word list correctly and in order, click
-                          **Continue.**. You will be asked to verify your phrase
-                          by clicking on the words in order. Once you have done
-                          that, press **Continue** again.
+                          You will be asked to verify your secret phrase by clicking on the words in the right order. After doing so, click on Continue.
                         </li>
                       </ul>
                       <ul className={styles.ul}>
                         <li className={styles.li}>
-                          Now it&apos;s time to create your username and
-                          password. Then click Accept PrivacyPolicy and Continue
-                          to finish.
+                          Now it&apos;s time to create your ZilPay username and password. Then Accept PrivacyPolicy and Continue to finish.
                         </li>
                       </ul>
                     </p>
                   </>
                 ) : (
                   <p className={styles.rowContentTxt}>
-                    To create your ZilPay wallet, you must first download the
-                    extension to your browser.
+                    Connect your Externally Owned Account for Zilliqa
                   </p>
                 )}
               </div>
             </div>
             <div className={styles.rowWrapper}>
-              <div className={styles.rowHeader}>
+              <div onClick={() => menuActive(2)} className={styles.rowHeader}>
                 <div className={styles.rowHeaderContent}>
                   <div>
-                    <Image alt="point-1" src={c2} width={25} height={25} />
+                    {isChecked(2) ? (
+                      <Image alt="point-1" src={cs} width={25} height={25} />
+                    ) : (
+                      <Image alt="point-1" src={c2} width={25} height={25} />
+                    )}
                   </div>
-                  <p className={styles.rowHeaderTitle}>
-                    Create ArConnect wallet
+                  <p className={styles.rowHeaderTitle}>Arweave</p>
+                </div>
+                <div>
+                  {active === 2 ? (
+                    <Image
+                      alt="arrow-up"
+                      src={ArrowUp}
+                      width={20}
+                      height={20}
+                    />
+                  ) : (
+                    <Image
+                      alt="arrow-down"
+                      src={ArrowDown}
+                      width={20}
+                      height={20}
+                    />
+                  )}
+                </div>
+              </div>
+              <div className={styles.rowContent}>
+                {active === 2 ? (
+                  <>
+                    <p className={styles.rowContentTxt}>
+                      Connect your Externally Owned Account for Arweave
+                    </p>
+                    <p className={styles.rowContentTxt}>
+                      <br />
+                      <br />
+                      <ul className={styles.ul}>
+                        <li className={styles.li}>
+                          Go to{" "}
+                          <a
+                            className={styles.linkColor}
+                            href="https://www.arconnect.io/ "
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            arconnect.io
+                          </a>{" "}
+                          and click on <strong>Download ArConnect</strong>
+                        </li>
+                      </ul>
+                      Once you have installed the chrome extension, a new tab will appear where you will be asked to create a password for your new Arweave wallet, called ArConnect.
+                      <br />
+                      <br />
+                      <ul className={styles.ul}>
+                        <li className={styles.li}>
+                          Generate your password, and click on Create.
+                        </li>
+                      </ul>
+                      {modalInfo && (
+                        <>
+                          <div
+                            onClick={() => setModalInfo(false)}
+                            className={styles.outerWrapper}
+                          />
+                          <div className={styles.modalInfo}>
+                            <h5 className={styles.modalInfoTitle}>INFO</h5>
+                            <p>
+                              Your SSI uses this wallet for encryption and decryption of data, and soon to make transactions on the permaweb as well!
+                            </p>
+                          </div>
+                        </>
+                      )}
+                      <ul className={styles.ul}>
+                        <li className={styles.li}>
+                          Next, select New Wallet
+                          <Image
+                            onClick={() => setModalInfo(!modalInfo)}
+                            alt="warning-ico"
+                            src={Warning}
+                            width={20}
+                            height={20}
+                          />
+                        </li>
+                      </ul>
+                    </p>
+                  </>
+                ) : (
+                  <p className={styles.rowContentTxt}>
+                    Connect your Externally Owned Account for Arweave
                   </p>
-                </div>
-                <div>
-                  <Image
-                    alt="arrow-down"
-                    src={ArrowDown}
-                    width={20}
-                    height={20}
-                  />
-                </div>
-              </div>
-              <div className={styles.rowContent}>
-                <p className={styles.rowContentTxt}>
-                  To create your ZilPay wallet, you must first download the
-                  extension to your browser.
-                </p>
-              </div>
-            </div>
-            <div className={styles.rowWrapper}>
-              <div className={styles.rowHeader}>
-                <div className={styles.rowHeaderContent}>
-                  <div>
-                    <Image alt="point-1" src={c3} width={25} height={25} />
-                  </div>
-                  <p className={styles.rowHeaderTitle}>Connect ZilPay wallet</p>
-                </div>
-                <div>
-                  <Image
-                    alt="arrow-down"
-                    src={ArrowDown}
-                    width={20}
-                    height={20}
-                  />
-                </div>
-              </div>
-              <div className={styles.rowContent}>
-                <p className={styles.rowContentTxt}>
-                  To create your ZilPay wallet, you must first download the
-                  extension to your browser.
-                </p>
-              </div>
-            </div>
-            <div className={styles.rowWrapper}>
-              <div className={styles.rowHeader}>
-                <div className={styles.rowHeaderContent}>
-                  <div>
-                    <Image alt="point-1" src={c4} width={25} height={25} />
-                  </div>
-                  <p className={styles.rowHeaderTitle}>Buy Tyron SSI</p>
-                </div>
-                <div>
-                  <Image
-                    alt="arrow-down"
-                    src={ArrowDown}
-                    width={20}
-                    height={20}
-                  />
-                </div>
-              </div>
-              <div className={styles.rowContent}>
-                <p className={styles.rowContentTxt}>
-                  To create your ZilPay wallet, you must first download the
-                  extension to your browser.
-                </p>
-              </div>
-            </div>
-            <div className={styles.rowWrapper}>
-              <div className={styles.rowHeader}>
-                <div className={styles.rowHeaderContent}>
-                  <div>
-                    <Image alt="point-1" src={c5} width={25} height={25} />
-                  </div>
-                  <p className={styles.rowHeaderTitle}>Buy NFT Username</p>
-                </div>
-                <div>
-                  <Image
-                    alt="arrow-down"
-                    src={ArrowDown}
-                    width={20}
-                    height={20}
-                  />
-                </div>
-              </div>
-              <div className={styles.rowContentNoBorder}>
-                <p className={styles.rowContentTxt}>
-                  To create your ZilPay wallet, you must first download the
-                  extension to your browser.
-                </p>
+                )}
               </div>
             </div>
           </div>
