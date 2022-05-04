9799205d9ef4ad8221320cd7fd23f52d574745c1
DEV PH <dev.ph@wistkey.com>
2022-05-04 09:02:11 +0700

    issue/can't access searchbar after search username

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -66,7 +66,7 @@ function Component() {
           if (VALID_SMART_CONTRACTS.includes(_username))
             window.open(
               SMART_CONTRACTS_URLS[
-                _username as unknown as keyof typeof SMART_CONTRACTS_URLS
+              _username as unknown as keyof typeof SMART_CONTRACTS_URLS
               ]
             );
           else
@@ -368,8 +368,6 @@ function Component() {
           className={styles.searchBar}
           onChange={handleOnChange}
           onKeyPress={handleOnKeyPress}
-          value={search}
-          placeholder={search}
           autoFocus
         />
         <div>
