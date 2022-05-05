d06bd1bf824f4372df05caaa5472b285a59926f7
DEV PH <dev.ph@wistkey.com>
2022-05-05 16:38:24 +0700

    issue/superposition newssi and buynft modal

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -57,15 +57,15 @@ function Header() {
       </div>
       {!menuOn && !modalTx && !modalDashboard && (
         <>
-          <NewSSIModal />
           <GetStartedModal />
           <BuyNFTModal />
           <AddFundsModal />
           <WithdrawalModal />
         </>
       )}
-      {!menuOn && !modalTx && <DashboardModal />}
       {!menuOn && <TransactionStatus />}
+      {!menuOn && !modalTx && <DashboardModal />}
+      {!menuOn && !modalTx && !modalBuyNft && <NewSSIModal />}
     </>
   );
 }
