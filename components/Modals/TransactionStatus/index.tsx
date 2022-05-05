c28186b6e8d5c53fd5ff98c3b0d1f60d51ff6614
DEV PH <dev.ph@wistkey.com>
2022-05-05 10:57:14 +0700

    @todo-i/transfer username parameters

--- components/Modals/TransactionStatus/index.tsx
+++ components/Modals/TransactionStatus/index.tsx
@@ -39,7 +39,7 @@ function TransactionStatus(props: ModalProps) {
         draggable: true,
         progress: undefined,
         theme: "dark",
-        toastId: 1
+        toastId: 1,
       });
     } else {
       updateModalTx(false);
@@ -59,19 +59,20 @@ function TransactionStatus(props: ModalProps) {
         {loading === "true"
           ? "Sign the transaction to send it to the blockchain"
           : loading === "submitted"
-            ? "Transaction processed on the Zilliqa blockchain - please wait"
-            : loading === "confirmed"
-              ? "Transaction successfully confirmed!"
-              : loading === "failed"
-                ? "Transaction failed"
-                : "Sign the transaction to send it to the blockchain"}
+          ? "Transaction processed on the Zilliqa blockchain - please wait"
+          : loading === "confirmed"
+          ? "Transaction successfully confirmed!"
+          : loading === "failed"
+          ? "Transaction failed"
+          : "Sign the transaction to send it to the blockchain"}
       </h5>
       {loading !== "true" && (
         <h5 style={{ fontSize: 14 }}>
           ID:{" "}
           <a
-            href={`https://devex.zilliqa.com/tx/${txId}?network=https%3A%2F%2F${net === "mainnet" ? "" : "dev-"
-              }api.zilliqa.com`}
+            href={`https://devex.zilliqa.com/tx/${txId}?network=https%3A%2F%2F${
+              net === "mainnet" ? "" : "dev-"
+            }api.zilliqa.com`}
             rel="noreferrer"
             target="_blank"
           >
