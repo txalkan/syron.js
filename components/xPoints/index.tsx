3ae9bcf019b96585465ef69b1b91127bb0b60a79
DEV PH <dev.ph@wistkey.com>
2022-05-19 17:21:58 +0700

    task/fetch xpoints, motionData

--- components/xPoints/index.tsx
+++ components/xPoints/index.tsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.scss'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import * as tyron from 'tyron'
 import { useStore } from 'effector-react'
 import Image from 'next/image'
@@ -25,13 +25,21 @@ function Component() {
     const user = useStore($user)
     const net = useStore($net)
     const [hideAdd, setHideAdd] = useState(true)
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
     const [showInput, setShowInput] = useState(false)
     const [addLegend, setAddLegend] = useState('new motion')
+    const [motionData, setMotionData] = useState(Array())
     const loginInfo = useSelector((state: RootState) => state.modal)
 
+    useEffect(() => {
+        fetchXpoints().then(() => {
+            fetchMotion().then(() => {
+                setLoading(false)
+            })
+        })
+    }, [])
+
     const fetchXpoints = async () => {
-        setLoading(true)
         updateXpointsBalance(0)
         let network = tyron.DidScheme.NetworkNamespace.Mainnet
         if (net === 'testnet') {
@@ -44,6 +52,7 @@ function Component() {
             _domain: 'did',
         })
             .then(async (donate_addr) => {
+                console.log('dd', donate_addr)
                 return await init.API.blockchain.getSmartContractSubState(
                     donate_addr,
                     'xpoints'
@@ -62,8 +71,54 @@ function Component() {
                 if (balance !== undefined) {
                     updateXpointsBalance(balance / 1e12)
                 }
+            })
+            .catch(() => {
                 setLoading(false)
-                updateNewMotionsModal(true)
+                throw new Error('Donate DApp: Not able to fetch balance.')
+            })
+    }
+
+    const fetchMotion = async () => {
+        let network = tyron.DidScheme.NetworkNamespace.Mainnet
+        if (net === 'testnet') {
+            network = tyron.DidScheme.NetworkNamespace.Testnet
+        }
+        const init = new tyron.ZilliqaInit.default(network)
+        await fetchAddr({
+            net,
+            _username: 'xpoints',
+            _domain: 'did',
+        })
+            .then(async (xpoint_addr) => {
+                const state = await init.API.blockchain
+                    .getSmartContractState(xpoint_addr)
+                    .then(async (state_) => {
+                        console.log(state_.result)
+                        let arr: any = []
+
+                        for (
+                            let i = 0;
+                            i < state_.result.motions.length;
+                            i += 1
+                        ) {
+                            const obj = {
+                                key: Object.keys(state_.result.motions[i])[0],
+                                motion: Object.values(
+                                    state_.result.motions[i]
+                                )[0],
+                                xp: Object.values(state_.result.ranking[i])[0],
+                            }
+                            arr = [obj, ...arr]
+                            console.log(obj)
+                        }
+
+                        var res = arr.sort((a, b) => b.xp - a.xp)
+                        setMotionData(res)
+                    })
+                    .catch((err: any) => {
+                        throw err
+                    })
+                return state
             })
             .catch(() => {
                 setLoading(false)
@@ -73,101 +128,109 @@ function Component() {
 
     return (
         <div style={{ textAlign: 'center', marginTop: '7%' }}>
-            <h1 style={{ marginBottom: '10%', color: '#ffff32' }}>
-                <span className={styles.x}>x</span>POINTS DApp
-            </h1>
-            {
-                //hideList &&
-                <div style={{ marginTop: '14%' }}>
-                    <h3 style={{ marginBottom: '7%', color: 'silver' }}>
-                        Raise Your Voice
-                    </h3>
-                    <div style={{ marginTop: '14%' }}>
-                        {hideAdd ? (
-                            <button
-                                type="button"
-                                className={styles.button}
-                                onClick={() => {
-                                    fetchXpoints()
-                                    // setHideAdd(false);
-                                    // setAddLegend("back");
-                                }}
-                            >
-                                {loading ? (
-                                    <i
-                                        className="fa fa-lg fa-spin fa-circle-notch"
-                                        aria-hidden="true"
-                                    ></i>
-                                ) : (
-                                    <p className={styles.buttonText}>
-                                        {addLegend}
-                                    </p>
-                                )}
-                            </button>
-                        ) : (
-                            <>
-                                <button
-                                    type="button"
-                                    className={styles.button}
-                                    onClick={() => {
-                                        setHideAdd(true)
-                                        setAddLegend('new motion')
-                                        //handleTest();
-                                    }}
-                                >
-                                    <p className={styles.buttonText}>
-                                        {addLegend}
-                                    </p>
-                                </button>
-                            </>
-                        )}
-                    </div>
-                </div>
-            }
-            {hideAdd && (
+            {loading ? (
+                <i
+                    className="fa fa-lg fa-spin fa-circle-notch"
+                    aria-hidden="true"
+                ></i>
+            ) : (
                 <>
-                    <div className={styles.wrapperMotion}>
-                        <div className={styles.motion}>
-                            <div className={styles.motionContent}>
-                                <div>
-                                    <div
-                                        onClick={() => setShowInput(!showInput)}
-                                        style={{ cursor: 'pointer' }}
+                    <h1 style={{ marginBottom: '10%', color: '#ffff32' }}>
+                        <span className={styles.x}>x</span>POINTS DApp
+                    </h1>
+                    {
+                        //hideList &&
+                        <div style={{ marginTop: '14%' }}>
+                            <h3 style={{ marginBottom: '7%', color: 'silver' }}>
+                                Raise Your Voice
+                            </h3>
+                            <div style={{ marginTop: '14%' }}>
+                                {hideAdd ? (
+                                    <button
+                                        type="button"
+                                        className={styles.button}
+                                        onClick={() => {
+                                            updateNewMotionsModal(true)
+                                            // setHideAdd(false);
+                                            // setAddLegend("back");
+                                        }}
                                     >
-                                        <Image alt='arrow' src={ArrowUp} />
+                                        <p className={styles.buttonText}>
+                                            {addLegend}
+                                        </p>
+                                    </button>
+                                ) : (
+                                    <>
+                                        <button
+                                            type="button"
+                                            className={styles.button}
+                                            onClick={() => {
+                                                setHideAdd(true)
+                                                setAddLegend('new motion')
+                                                //handleTest();
+                                            }}
+                                        >
+                                            <p className={styles.buttonText}>
+                                                {addLegend}
+                                            </p>
+                                        </button>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                    }
+                    {hideAdd && (
+                        <>
+                            <div className={styles.wrapperMotion}>
+                                <div className={styles.motion}>
+                                    <div className={styles.motionContent}>
+                                        <div>
+                                            <div
+                                                onClick={() =>
+                                                    setShowInput(!showInput)
+                                                }
+                                                style={{ cursor: 'pointer' }}
+                                            >
+                                                <Image
+                                                    alt="arrow"
+                                                    src={ArrowUp}
+                                                />
+                                            </div>
+                                            <h3>64</h3>
+                                        </div>
+                                        <div className={styles.motionTxt}>
+                                            Lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit. Fusce
+                                            sollicitudin vestibulum odio ac
+                                            congue. Quisque convallis
+                                            sollicitudin semper.
+                                        </div>
                                     </div>
-                                    <h3>64</h3>
-                                </div>
-                                <div className={styles.motionTxt}>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Fusce sollicitudin
-                                    vestibulum odio ac congue. Quisque convallis
-                                    sollicitudin semper.
+                                    {showInput && (
+                                        <div className={styles.inputWrapper}>
+                                            <input
+                                                style={{ marginBottom: '10%' }}
+                                                type="text"
+                                                placeholder="type number to xPoints to add"
+                                                // onChange={handleInput}
+                                                // onKeyPress={handleOnKeyPress}
+                                                autoFocus
+                                            />
+                                            <input
+                                                style={{ marginLeft: '2%' }}
+                                                type="button"
+                                                className={'button secondary'}
+                                                value={'Vote'}
+                                                // onClick={() => {
+                                                //     handleSave()
+                                                // }}
+                                            />
+                                        </div>
+                                    )}
                                 </div>
                             </div>
-                            {showInput && (
-                                <div className={styles.inputWrapper}>
-                                    <input
-                                        style={{ marginBottom: '10%' }}
-                                        type="text"
-                                        placeholder="type number to xPoints to add"
-                                        // onChange={handleInput}
-                                        // onKeyPress={handleOnKeyPress}
-                                        autoFocus
-                                    />
-                                    <input
-                                        style={{ marginLeft: '2%' }}
-                                        type="button"
-                                        className={'button secondary'}
-                                        value={'Vote'}
-                                    // onClick={() => {
-                                    //     handleSave()
-                                    // }}
-                                    />
-                                </div>
-                            )}
-                        </div>
-                    </div>
+                        </>
+                    )}
                 </>
             )}
         </div>
