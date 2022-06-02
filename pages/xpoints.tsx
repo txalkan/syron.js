a0ab3fa848e36fa80571fa9aa718c9a525b2cc02
DEV PH <dev.ph@wistkey.com>
2022-06-02 09:55:33 +0700

    task/add headline on xpoints

--- pages/xpoints.tsx
+++ pages/xpoints.tsx
@@ -1,10 +1,26 @@
 import Layout from '../components/Layout'
-import { XPoints } from '../components'
+import { Headline, XPoints } from '../components'
 
 function Component() {
+    const data = [
+        {
+            name: 'did',
+            router: '',
+        },
+    ]
+
     return (
         <>
             <Layout>
+                <div
+                    style={{
+                        width: '100%',
+                        marginTop: '10%',
+                        marginBottom: '-10%',
+                    }}
+                >
+                    <Headline data={data} />
+                </div>
                 <XPoints />
             </Layout>
         </>
