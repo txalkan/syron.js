0bc614d2d51062937f6a12d99895ebb595fe8318
tralkan <tralkan@ssiprotocol.com>
2022-09-13 23:22:45 -0300

    update xwallets

---
+++ pages/vcxwallet/nft copy/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '../../../components/Layout'
+import { VerifiableCredentials } from '../../../components'
+
+function Header() {
+    return (
+        <>
+            <Layout>
+                <VerifiableCredentials />
+            </Layout>
+        </>
+    )
+}
+
+export default Header
