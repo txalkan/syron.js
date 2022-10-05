71b7bbc778ba1568ef3d6ed29dfda96e99cb66ad
devwitkey <dev.ph@wistkey.com>
2022-10-05 14:25:51 +0700

    create ssi mobile

--- components/Modals/DashboardModal/index.tsx
+++ components/Modals/DashboardModal/index.tsx
@@ -284,12 +284,12 @@ function Component() {
                                 )
                             }, 1000)
                             let new_ssi = deploy[0].ContractAddress
-                            new_ssi = zcrypto.toChecksumAddress(new_ssi)
+                            // new_ssi = zcrypto.toChecksumAddress(new_ssi)
                             updateBuyInfo(null)
                             dispatch(updateLoginInfoUsername(null!))
                             dispatch(
                                 updateLoginInfoAddress(
-                                    zcrypto.toChecksumAddress(new_ssi)
+                                    new_ssi
                                 )
                             )
                             updateDashboardState('loggedIn')
@@ -298,31 +298,7 @@ function Component() {
                             Router.push('/address')
                         } else if (tx.isRejected()) {
                             // setLoadingSsi(false)
-                            setTimeout(async () => {
-                                tx = await tx.confirm(deploy[0].ID, 33)
-                                if (tx.isConfirmed()) {
-                                    dispatch(setTxStatusLoading('confirmed'))
-                                    setTimeout(() => {
-                                        window.open(
-                                            `https://v2.viewblock.io/zilliqa/tx/${deploy[0].ID}?network=${net}`
-                                        )
-                                    }, 1000)
-                                    let new_ssi = deploy[0].ContractAddress
-                                    new_ssi = zcrypto.toChecksumAddress(new_ssi)
-                                    updateBuyInfo(null)
-                                    dispatch(updateLoginInfoUsername(null!))
-                                    dispatch(
-                                        updateLoginInfoAddress(
-                                            zcrypto.toChecksumAddress(new_ssi)
-                                        )
-                                    )
-                                    updateDashboardState('loggedIn')
-                                    updateModalBuyNft(false)
-                                    Router.push('/address')
-                                } else if (tx.isRejected()) {
-                                    dispatch(setTxStatusLoading('failed'))
-                                }
-                            }, 1000)
+                            dispatch(setTxStatusLoading('failed'))
                         }
                     })
                     .catch((error) => {
