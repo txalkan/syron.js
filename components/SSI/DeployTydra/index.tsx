ece3a1e482cf96c4ec7dafaffc1e4b6d69f2cd7a
devwitkey <dev.ph@wistkey.com>
2022-10-27 11:47:29 +0700

    send tydra tx

--- components/SSI/DeployTydra/index.tsx
+++ components/SSI/DeployTydra/index.tsx
@@ -13,7 +13,6 @@ import { ZilPayBase } from '../../ZilPay/zilpay-base'
 import { setTxId, setTxStatusLoading } from '../../../src/app/actions'
 import { updateModalTx, updateModalTxMinimized } from '../../../src/store/modal'
 import ThreeDots from '../../Spinner/ThreeDots'
-import Arweave from 'arweave'
 import Tydra from '../../../src/assets/logos/tydra.json'
 import arweave from '../../../src/config/arweave'
 
@@ -29,8 +28,11 @@ function Component() {
     const resolvedInfo = useStore($resolvedInfo)
     const [isDeployed, setIsDeployed] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
+    const [tokenUri, setTokenUri] = useState('')
 
     const submitAr = async () => {
+        setIsDeployed(false)
+        setTokenUri('')
         setIsLoading(true)
         try {
             const data = {
@@ -47,7 +49,8 @@ function Component() {
             transaction.addTag('Content-Type', 'application/json')
 
             window.arweaveWallet.dispatch(transaction).then((res) => {
-                console.log(res)
+                setTokenUri(res.id)
+                sendTxn()
             })
         } catch (err) {
             console.log(err)
@@ -55,16 +58,78 @@ function Component() {
         setIsLoading(false)
     }
 
+    const sendTxn = async () => {
+        setIsLoading(true)
+        const zilpay = new ZilPayBase()
+        let tx = await tyron.Init.default.transaction(net)
+        const domainId = '0x' + (await tyron.Util.default.HashString(resolvedInfo?.name!))
+        console.log(domainId)
+        let params: any = []
+        const id = {
+            vname: 'id',
+            type: 'String',
+            value: 'zil',
+        }
+        params.push(id)
+        const token_id = {
+            vname: 'token_id',
+            type: 'ByStr32',
+            value: domainId,
+        }
+        params.push(token_id)
+        const token_uri = {
+            vname: 'token_uri',
+            type: 'ByStr32',
+            value: tokenUri,
+        }
+        params.push(token_uri)
+
+        dispatch(setTxStatusLoading('true'))
+            updateModalTxMinimized(false)
+            updateModalTx(true)
+            await zilpay
+                .call({
+                    contractAddress: resolvedInfo?.addr!,
+                    transition: 'MintTydraNft',
+                    params: params as unknown as Record<string, unknown>[],
+                    amount: String(0),
+                })
+                .then(async (res) => {
+                    dispatch(setTxId(res.ID))
+                    dispatch(setTxStatusLoading('submitted'))
+                    tx = await tx.confirm(res.ID)
+                    if (tx.isConfirmed()) {
+                        setIsLoading(false)
+                        setIsDeployed(false)
+                        dispatch(setTxStatusLoading('confirmed'))
+                        setTimeout(() => {
+                            window.open(
+                                `https://v2.viewblock.io/zilliqa/tx/${res.ID}?network=${net}`
+                            )
+                        }, 1000)
+                    } else if (tx.isRejected()) {
+                        setIsLoading(false)
+                        dispatch(setTxStatusLoading('failed'))
+                    }
+                })
+                .catch((err) => {
+                    setIsLoading(false)
+                    dispatch(setTxStatusLoading('rejected'))
+                    updateModalTxMinimized(false)
+                    updateModalTx(true)
+                    throw err
+                })
+    }
+
     return (
         <div className={styles.cardActiveWrapper}>
             <div
                 onClick={() => {
-                    submitAr()
-                    // setIsLoading(true)
-                    // setTimeout(() => {
-                    //     setIsDeployed(!isDeployed)
-                    //     setIsLoading(false)
-                    // }, 1000)
+                    if (isDeployed) {
+                        sendTxn()
+                    } else {
+                        submitAr()
+                    }
                 }}
                 className={isDeployed ? styles.cardDeployed : styles.card}
             >
