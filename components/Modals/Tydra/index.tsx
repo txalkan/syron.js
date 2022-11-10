14542322ff34753b6b1bc7ba0cf6b4d0c15909ea
devwitkey <dev.ph@wistkey.com>
2022-11-10 09:44:15 +0700

    transfer tydra

--- components/Modals/Tydra/index.tsx
+++ components/Modals/Tydra/index.tsx
@@ -18,7 +18,7 @@ import * as tyron from 'tyron'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../../src/app/reducers'
 import Spinner from '../../Spinner'
-import { AddFunds, Donate, Selector } from '../..'
+import { AddFunds, Donate, SearchBarWallet, Selector } from '../..'
 import { useTranslation } from 'next-i18next'
 import smartContract from '../../../src/utils/smartContract'
 import routerHook from '../../../src/hooks/router'
@@ -35,6 +35,7 @@ import { toast } from 'react-toastify'
 import toastTheme from '../../../src/hooks/toastTheme'
 
 function Component() {
+    const zcrypto = tyron.Util.default.Zcrypto()
     const { t } = useTranslation()
     const { getSmartContract } = smartContract()
     const { navigate } = routerHook()
@@ -51,16 +52,32 @@ function Component() {
     const version = parseInt(resolvedInfo?.version?.split('_')[1]!)
 
     const [currency, setCurrency] = useState('')
+    const [tydra, setTydra] = useState('')
     const [txName, setTxName] = useState('')
     const [res, setRes] = useState('')
     const [isLoading, setIsLoading] = useState(false)
     const [isEnough, setIsEnough] = useState(true)
+    const [loading, setLoading] = useState(false)
+    const [addr, setAddr] = useState('')
+    const [savedAddr, setSavedAddr] = useState(false)
+    const [usernameInput, setUsernameInput] = useState('')
     const username = resolvedInfo?.name
     const domain = resolvedInfo?.domain
     const domainNavigate = domain !== '' ? domain + '@' : ''
 
     const handleOnChange = (value) => {
+        updateDonation(null)
+        setTydra('')
+        setSavedAddr(false)
+        setAddr('')
         setCurrency(value)
+    }
+
+    const handleOnChangeTydra = (value) => {
+        updateDonation(null)
+        setAddr('')
+        setSavedAddr(false)
+        setTydra(value)
     }
 
     const submitAr = async () => {
@@ -143,7 +160,7 @@ function Component() {
         }
     }
 
-    const sendTxn = async () => {
+    const handleSubmitSend = async () => {
         setIsEnough(true)
         setIsLoading(true)
         let freeList = false
@@ -282,10 +299,158 @@ function Component() {
         }
     }
 
+    const handleSubmitTransfer = async () => {
+        setIsLoading(true)
+        let freeList = false
+        const init_addr = await tyron.SearchBarUtil.default.fetchAddr(
+            net,
+            'init',
+            'did'
+        )
+        let params: any = []
+        let contract = init_addr
+        let currency_ = 'zil'
+        let price = 500
+        if (version >= 6) {
+            price = 1000
+            currency_ = currency.toLowerCase()
+            contract = resolvedInfo?.addr!
+            const donation_ = await tyron.Donation.default.tyron(donation!)
+            const tyron_ = {
+                vname: 'tyron',
+                type: 'Option Uint128',
+                value: donation_,
+            }
+            params.push(tyron_)
+        }
+        switch (currency_.toLowerCase()) {
+            case 'tyron':
+                price = 30
+                break
+            case '$si':
+                price = 30
+                break
+            case 'zusdt':
+                price = 30
+                break
+            case 'xsgd':
+                price = 40
+                break
+            case 'xidr':
+                price = 150000
+                break
+        }
+        const balance = await fetchZilBalance(currency_.toLowerCase())
+        if (price > balance) {
+            setIsLoading(false)
+            setIsEnough(false)
+            toast.error(
+                `Insufficient balance, the cost is ${price} ${currency_}`,
+                {
+                    position: 'top-center',
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: toastTheme(isLight),
+                    toastId: 2,
+                }
+            )
+        } else {
+            try {
+                const get_free_list = await getSmartContract(
+                    init_addr,
+                    'tydra_free_list'
+                )
+                const freelist: Array<string> =
+                    get_free_list.result.tydra_free_list
+                const is_free = freelist.filter(
+                    (val) => val === loginInfo.zilAddr.base16.toLowerCase()
+                )
+                if (is_free.length !== 0) {
+                    freeList = true
+                }
+            } catch {
+                freeList = false
+            }
+            const zilpay = new ZilPayBase()
+            let tx = await tyron.Init.default.transaction(net)
+            const domainId =
+                '0x' +
+                (await tyron.Util.default.HashString(resolvedInfo?.name!))
+            const domainIdTo =
+                '0x' + (await tyron.Util.default.HashString(addr!))
+            const id = {
+                vname: 'id',
+                type: 'String',
+                value: freeList ? 'free' : currency_.toLowerCase(),
+            }
+            params.push(id)
+            const tydra_ = {
+                vname: 'tydra',
+                type: 'String',
+                value: tydra,
+            }
+            params.push(tydra_)
+            const token_id = {
+                vname: 'token_id',
+                type: 'ByStr32',
+                value: domainId,
+            }
+            params.push(token_id)
+            const to_token_id = {
+                vname: 'to_token_id',
+                type: 'ByStr32',
+                value: domainIdTo,
+            }
+            params.push(to_token_id)
+
+            dispatch(setTxStatusLoading('true'))
+            updateModalTxMinimized(false)
+            updateModalTx(true)
+            await zilpay
+                .call({
+                    contractAddress: contract,
+                    transition: 'TransferTydraNft',
+                    params: params as unknown as Record<string, unknown>[],
+                    amount: String(freeList ? 0 : price),
+                })
+                .then(async (res) => {
+                    dispatch(setTxId(res.ID))
+                    dispatch(setTxStatusLoading('submitted'))
+                    tx = await tx.confirm(res.ID)
+                    if (tx.isConfirmed()) {
+                        setIsLoading(false)
+                        dispatch(setTxStatusLoading('confirmed'))
+                        setTimeout(() => {
+                            window.open(
+                                `https://v2.viewblock.io/zilliqa/tx/${res.ID}?network=${net}`
+                            )
+                        }, 1000)
+                        updateTydraModal(false)
+                        navigate(`/${domainNavigate}${username}/didx`)
+                    } else if (tx.isRejected()) {
+                        setIsLoading(false)
+                        dispatch(setTxStatusLoading('failed'))
+                    }
+                })
+                .catch((err) => {
+                    setIsLoading(false)
+                    dispatch(setTxStatusLoading('rejected'))
+                    updateModalTxMinimized(false)
+                    updateModalTx(true)
+                    throw err
+                })
+        }
+    }
+
     const toggleActive = (id: string) => {
-        // updateDonation(null)
+        updateDonation(null)
         // resetState()
         setCurrency('')
+        setRes('')
         if (id === txName) {
             setTxName('')
         } else {
@@ -300,6 +465,60 @@ function Component() {
             setRes('')
             updateTydraModal(false)
         }
+    }
+
+    const resolveUsername = async () => {
+        setLoading(true)
+        const input = usernameInput.replace(/ /g, '')
+        let username = input.toLowerCase()
+        let domain = ''
+        if (input.includes('@')) {
+            username = input
+                .split('@')[1]
+                .replace('.did', '')
+                .replace('.ssi', '')
+                .toLowerCase()
+            domain = input.split('@')[0]
+        } else if (input.includes('.')) {
+            if (input.split('.')[1] === 'did') {
+                username = input.split('.')[0].toLowerCase()
+                domain = 'did'
+            } else if (input.split('.')[1] === 'ssi') {
+                username = input.split('.')[0].toLowerCase()
+            } else {
+                throw Error()
+            }
+        }
+        const domainId = '0x' + (await tyron.Util.default.HashString(username))
+        await tyron.SearchBarUtil.default
+            .fetchAddr(net, domainId, domain)
+            .then(async (addr) => {
+                addr = zcrypto.toChecksumAddress(addr)
+                setAddr(addr)
+                setSavedAddr(true)
+            })
+            .catch(() => {
+                toast.error('Identity verification unsuccessful.', {
+                    position: 'top-right',
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: toastTheme(isLight),
+                })
+            })
+        setLoading(false)
+    }
+
+    const handleInput = ({
+        currentTarget: { value },
+    }: React.ChangeEvent<HTMLInputElement>) => {
+        updateDonation(null)
+        setSavedAddr(false)
+        setAddr('')
+        setUsernameInput(value)
     }
 
     const renderSend = () => {
@@ -339,6 +558,21 @@ function Component() {
         },
     ]
 
+    const optionCurrencyTransfer = [
+        {
+            key: 'FREE',
+            name: 'FREE',
+        },
+        ...optionCurrency,
+    ]
+
+    const optionTydra = [
+        {
+            key: 'nawelito',
+            name: 'Nawelito',
+        },
+    ]
+
     if (!modalTydra) {
         return null
     }
@@ -357,9 +591,7 @@ function Component() {
                                 height={15}
                             />
                         </div>
-                        <h5 className={styles.headerTxt}>
-                            Deploy TYDRA{currency}
-                        </h5>
+                        <h5 className={styles.headerTxt}>Deploy TYDRA</h5>
                     </div>
                     <div className={styles.cardWrapper}>
                         <div
@@ -438,7 +670,9 @@ function Component() {
                                                         }
                                                     >
                                                         <div
-                                                            onClick={sendTxn}
+                                                            onClick={
+                                                                handleSubmitSend
+                                                            }
                                                             className={
                                                                 isLight
                                                                     ? 'actionBtnLight'
@@ -460,31 +694,112 @@ function Component() {
                             )}
                         </div>
                     </div>
-                    {/* <div className={styles.contentWrapper}>
-                        <div className={styles.select}>
-                            <Selector
-                                option={option}
-                                onChange={handleOnChange}
-                                placeholder={t('Select coin')}
-                            />
+                    <div className={styles.cardWrapper}>
+                        <div
+                            onClick={() => toggleActive('transferTydra')}
+                            className={
+                                txName === 'transferTydra'
+                                    ? styles.cardActive
+                                    : styles.card
+                            }
+                        >
+                            <div>TRANSFER TYDRA</div>
                         </div>
-                        {currency !== '' && (
-                            <div className={styles.btnWrapper}>
-                                <div
-                                    onClick={submitAr}
-                                    className={
-                                        isLight ? 'actionBtnLight' : 'actionBtn'
-                                    }
-                                >
-                                    {isLoading ? (
-                                        <ThreeDots color="basic" />
-                                    ) : (
-                                        'DEPLOY TYDRA'
+                        <div className={styles.cardActiveWrapper}>
+                            {txName === 'transferTydra' && (
+                                <div className={styles.cardRight}>
+                                    <div className={styles.closeIcoWrapper}>
+                                        <div
+                                            onClick={() => toggleActive('')}
+                                            className={styles.closeIco}
+                                        >
+                                            <Image
+                                                width={10}
+                                                src={CloseIco}
+                                                alt="close-ico"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className={styles.picker}>
+                                        <Selector
+                                            option={optionCurrencyTransfer}
+                                            onChange={handleOnChange}
+                                            placeholder={t('Select coin')}
+                                        />
+                                    </div>
+                                    {currency !== '' && (
+                                        <>
+                                            <div className={styles.picker}>
+                                                <Selector
+                                                    option={optionTydra}
+                                                    onChange={
+                                                        handleOnChangeTydra
+                                                    }
+                                                    placeholder={t(
+                                                        'Select Tydra'
+                                                    )}
+                                                />
+                                            </div>
+                                            {tydra !== '' && (
+                                                <>
+                                                    <div
+                                                        className={
+                                                            styles.picker
+                                                        }
+                                                    >
+                                                        <SearchBarWallet
+                                                            resolveUsername={
+                                                                resolveUsername
+                                                            }
+                                                            handleInput={
+                                                                handleInput
+                                                            }
+                                                            input={
+                                                                usernameInput
+                                                            }
+                                                            loading={loading}
+                                                            saved={savedAddr}
+                                                        />
+                                                    </div>
+                                                    {savedAddr && (
+                                                        <>
+                                                            {version >= 6 && (
+                                                                <Donate />
+                                                            )}
+                                                            {renderSend() && (
+                                                                <div
+                                                                    className={
+                                                                        styles.btnWrapper
+                                                                    }
+                                                                >
+                                                                    <div
+                                                                        onClick={
+                                                                            handleSubmitTransfer
+                                                                        }
+                                                                        className={
+                                                                            isLight
+                                                                                ? 'actionBtnLight'
+                                                                                : 'actionBtn'
+                                                                        }
+                                                                    >
+                                                                        {isLoading ? (
+                                                                            <ThreeDots color="basic" />
+                                                                        ) : (
+                                                                            'TRANSFER TYDRA'
+                                                                        )}
+                                                                    </div>
+                                                                </div>
+                                                            )}
+                                                        </>
+                                                    )}
+                                                </>
+                                            )}
+                                        </>
                                     )}
                                 </div>
-                            </div>
-                        )}
-                    </div> */}
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
