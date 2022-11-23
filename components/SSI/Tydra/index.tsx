b36131445d81ad489e008268da2eea0bfda4d2af
tralkan <tralkan@ssiprotocol.com>
2022-11-23 18:28:13 -0300

    resolve nft dns & tydras

--- components/SSI/Tydra/index.tsx
+++ components/SSI/Tydra/index.tsx
@@ -20,15 +20,15 @@ function Component() {
     const resolvedInfo = useStore($resolvedInfo)
     const [loadingTydra, setLoadingTydra] = useState(true)
     const [tydra, setTydra] = useState('')
-    const [isNawelito, setIsNawelito] = useState(true)
+    const [isTydra, setIsTydra] = useState(true)
     const [baseUri, setBaseUri] = useState(true)
     const [tokenUri, setTokenUri] = useState('')
     const version = checkVersion(resolvedInfo?.version)
 
     const checkType = async () => {
-        setIsNawelito(true)
+        setIsTydra(true)
         if (version < 6) {
-            fetchTydra()
+            fetchTydra('')
         } else {
             try {
                 const domainId =
@@ -46,11 +46,65 @@ function Component() {
                 const nftDns = await tyron.SmartUtil.default.intoMap(
                     get_nftDns.result.nft_dns
                 )
-                const nftDns_ = nftDns.get(resolvedInfo?.domain!)
-                if (nftDns_ === 'nawelito') {
-                    fetchTydra()
+                let subdomain = resolvedInfo?.domain!
+                if (subdomain === '') {
+                    subdomain = 'ssi'
+                }
+                const nftDns_ = nftDns.get(subdomain)
+                console.log('nft_dns', nftDns_)
+                const collection = nftDns_.split('#')[0]
+                if (
+                    collection === 'nawelito' ||
+                    collection === 'nawelitoonfire' ||
+                    collection === 'nessy'
+                ) {
+                    // @todo-i add if nawelitoonfire & nessy
+                    fetchTydra(nftDns_)
+                    updateLoadingTydra(true)
+                    setLoadingTydra(true)
+                    try {
+                        const init_addr =
+                            await tyron.SearchBarUtil.default.fetchAddr(
+                                net,
+                                'init',
+                                'did'
+                            )
+                        const base_uri = await getSmartContract(
+                            init_addr,
+                            'base_uri'
+                        )
+                        const baseUri = base_uri.result.base_uri
+                        // const get_tokenuri = await getSmartContract(init_addr, 'token_uris')
+                        // const token_uris = await tyron.SmartUtil.default.intoMap(
+                        //     get_tokenuri.result.token_uris
+                        // )
+                        // const arr = Array.from(token_uris.values())
+                        // const domainId =
+                        //     '0x' +
+                        //     (await tyron.Util.default.HashString(resolvedInfo?.name!))
+                        // let tokenUri = arr[0][domainId] //@todo-i this is the case if nawelito
+                        // if (!tokenUri) {
+                        //     tokenUri = arr[1][domainId] // nawelito on fire
+                        // }
+                        // if (!tokenUri) {
+                        //     tokenUri = arr[2][domainId] // nessy @todo-i look in the token_uris by ID
+                        // }
+                        // console.log('tydra', tokenUri)
+                        await fetch(`${baseUri}${tokenUri}`)
+                            .then((response) => response.json())
+                            .then((data) => {
+                                setLoadingTydra(false)
+                                setTimeout(() => {
+                                    updateLoadingTydra(false)
+                                }, 3000)
+                                setTydra(data.resource)
+                            })
+                    } catch (err) {
+                        setLoadingTydra(false)
+                        updateLoadingTydra(false)
+                    }
                 } else if (nftDns_) {
-                    setIsNawelito(false)
+                    setIsTydra(false)
                     fetchOtherNft(nftDns_)
                 } else {
                     console.log('nftDns not found')
@@ -60,12 +114,11 @@ function Component() {
                     }, 3000)
                 }
             } catch {
-                fetchTydra()
+                fetchTydra('')
             }
         }
     }
 
-    //@todo-i-fixed add toast for error
     const fetchOtherNft = async (nftName: string) => {
         try {
             const init_addr = await tyron.SearchBarUtil.default.fetchAddr(
@@ -90,6 +143,8 @@ function Component() {
                 get_tokenUris.result.token_uris
             )
             console.log('@', tokenUris)
+
+            //@todo-i add condition to verify that the DIDxWallet (username.did) or ZilPay wallet is the token owner for the given ID
             const tokenUris_ = tokenUris.get(nftName.split('#')[1])
             setTokenUri(tokenUris_)
             setLoadingTydra(false)
@@ -101,8 +156,8 @@ function Component() {
             setTimeout(() => {
                 updateLoadingTydra(false)
             }, 3000)
-            toast.error('Failed to fetch NFT', {
-                position: 'top-center',
+            toast.error('Failed to verify NFT', {
+                position: 'bottom-right',
                 autoClose: 3000,
                 hideProgressBar: false,
                 closeOnClick: true,
@@ -115,7 +170,7 @@ function Component() {
         }
     }
 
-    const fetchTydra = async () => {
+    const fetchTydra = async (nftDns: string) => {
         updateLoadingTydra(true)
         setLoadingTydra(true)
         try {
@@ -134,12 +189,24 @@ function Component() {
             const domainId =
                 '0x' +
                 (await tyron.Util.default.HashString(resolvedInfo?.name!))
-            let tokenUri = arr[0][domainId]
-            if (!tokenUri) {
-                tokenUri = arr[1][domainId]
-            }
-            if (!tokenUri) {
-                tokenUri = arr[2][domainId]
+            let tokenUri
+            if (version < 6) {
+                tokenUri = arr[0][domainId]
+                if (!tokenUri) {
+                    tokenUri = arr[1][domainId]
+                }
+                if (!tokenUri) {
+                    tokenUri = arr[2][domainId]
+                }
+            } else {
+                const collection = nftDns.split('#')[0]
+                if (collection === 'nawelito') {
+                    tokenUri = arr[0][domainId]
+                } else if (collection === 'nawelitoonfire') {
+                    tokenUri = arr[1][domainId]
+                } else if (collection === 'nessy') {
+                    tokenUri = arr[2][domainId]
+                }
             }
             console.log('tydra', tokenUri)
             await fetch(`${baseUri}${tokenUri}`)
@@ -170,7 +237,7 @@ function Component() {
                 </div>
             ) : (
                 <>
-                    {isNawelito ? (
+                    {isTydra ? (
                         <>
                             {tydra !== '' && (
                                 <img
