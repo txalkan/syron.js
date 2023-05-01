df7f6869c148762d2353573961bf612ab9654772
tralkan <tralkan@ssiprotocol.com>
2023-05-01 12:41:02 -0300

    remove @ from valid username

--- src/constants/mintDomainName.ts
+++ src/constants/mintDomainName.ts
@@ -34,7 +34,31 @@ export const optionPayment = [
     },
 ]
 
-const pattern = /^[^\s.:/?#]*$/u
+const pattern = /^[^\s.:/?@#]*$/u
+/*
+The condition you provided is a regular expression pattern that matches any string that does not contain whitespace or certain characters often used in URLs.
+
+Here's a breakdown of the pattern:
+
+/ - start of the regular expression pattern
+^ - start of the string
+[^\s.:/?#]* - any number of characters that are not whitespace (\s) or any of the following characters: ., :, /, ?, #
+$ - end of the string
+/ - end of the regular expression pattern
+u - flag indicating that the pattern should be interpreted as a Unicode string
+So, for example, the following strings would match this pattern:
+
+"hello"
+"abc123"
+"!@#$%"
+"こんにちは"
+"مرحبا"
+And the following strings would not match:
+
+"hello world" (contains a space)
+"http://example.com" (contains : and /)
+"example.com" (contains .)
+*/
 export const isValidUsername = (domain: string) =>
     (pattern.test(domain) && //(/^[^\s.:/?#\p{L}\p{M}]*$/.test(domain)
         domain.length > 4) ||
