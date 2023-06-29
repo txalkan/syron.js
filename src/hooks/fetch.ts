4833a3f3844ed14b5247de6e14a8e2ce01ebeb52
tralkan <tralkan@ssiprotocol.com>
2023-06-29 13:52:19 -0300

    update node fetch

--- src/hooks/fetch.ts
+++ src/hooks/fetch.ts
@@ -398,7 +398,7 @@ function fetch() {
                 let tokenUri = arr[id][domainId]
                 let token_uris_: any = []
                 if (tokenUri) {
-                    await fetchNode(`${baseUri}${tokenUri}`)
+                    await fetchNode.default(`${baseUri}${tokenUri}`)
                         .then((response) => response.json())
                         .then((data) => {
                             const obj = {
