89d890c868ec5a2872904c831511fcd3e9ceadcd
Xalkan Duarte <xalkan@pm.me>
2025-10-15 21:24:36 -0300

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,6 @@
-const path = require('path')
 module.exports = {
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'es', 'cn', 'id', 'ru'],
-        localePath: path.resolve('./public/locales'),
     },
 }
